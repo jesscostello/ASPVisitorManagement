422cea8dd8dd48db92830b929b5cb525cb59b7b4
Jess Costello <jessica.a.costello@gmail.com>
2018-07-23 11:33:27 +1200

    Created models and db context

---
+++ ASPVisitorManagement/Data/VisitorDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ASPVisitorManagement.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPVisitorManagement.Data
+{
+    public class VisitorDbContext : DbContext
+    {
+        // Here we add in all the tables we are using.
+        public DbSet<StaffNames> StaffNames { get; set; }
+        public DbSet<Visitor> Visitor { get; set; }
+        
+        // We need 2 constructors, one is empty, and the other injects in DbContextOptions
+        public VisitorDbContext(DbContextOptions<VisitorDbContext> options)
+            : base(options)
+        {
+        }
+
+        public VisitorDbContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
